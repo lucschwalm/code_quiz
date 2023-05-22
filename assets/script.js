edbf6cca15b0badaad72e9ad2a288e13a9657b4a
lucschwalm <lucschwalm03@gmail.com>
2023-05-22 16:07:10 -0500

    Added more questions and selecting an answer goes to the next question

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,34 @@ var questionText = document.getElementById("mainText");
 var main = document.getElementById("main");
 var answerButtons = document.getElementById("answerButtons");
 var timer = document.getElementById("timer");
+var isCorrect = document.getElementById("isCorrect");
+
 var question1 = "Commonly used data types do not include:"
-var questions = [question1];
+var question2 = "The condition in an if/else statement is enclosed by:"
+var question3 = "Properties within an object are accessed using: ";
+var question4 = "Arrays are enclosed within: ";
+var question5 = "An html element can be accessed within javascript using all but: ";
+var questions = [question1, question2, question3, question4, question5];
 var answers1 = ["strings", "booleans", "alerts", "numbers"];
-var questionAnswers = [answers1]; 
+var answers2 = ["parenthesis", "curly brackets", "brackets", "quotation marks"];
+var answers3 = ["types", "keys", "functions", "variables"];
+var answers4 = ["brackets", "asterisks", "parenthesis", "semicolons"];
+var answers5 = ["getElementbyID()", "getElementsbyClassName()", "getElementsbyTagName()", "getElement()"];
+var questionAnswers = [answers1, answers2, answers3, answers4, answers5]; 
+var correctAnswers = ["alerts", "parenthesis", "keys", "brackets", "getElement()"];
+var questionNum = 0;
 
 function nextQuestion() {
-    start.style.display = "none";
+    if(questionNum >= questions.length){
+        isCorrect.textContent = "You win!";
+        return;
+    }
     questionText.textContent = questions[questionNum];
+    if(answerButtons.hasChildNodes()) {
+        for(var i=0; i<4; i++){
+            answerButtons.removeChild(answerButtons.children[0]);
+        }
+    }
     for(var i=0; i<4; i++){
         var button = document.createElement("button");
         var answers = questionAnswers[questionNum];
@@ -28,8 +48,8 @@ function decreaseTime() {
     }, 1000);
 }
 
-questionNum = 0;
 start.addEventListener("click", function() {
+    start.style.display = "none";
     nextQuestion();
     decreaseTime();
 });
@@ -38,14 +58,14 @@ answerButtons.addEventListener("click", function(event) {
     var element = event.target;
     console.log(element);
     if(element.matches("button") === true) {
-        if(element.textContent == "alerts"){
-            var isCorrect = document.createElement("h4");
+        if(element.textContent == correctAnswers[questionNum]){
             isCorrect.textContent = "Correct";
-            main.appendChild(isCorrect);
+            questionNum++;
+            nextQuestion();
         }else{
-            var isCorrect = document.createElement("h4");
             isCorrect.textContent = "Incorrect";
-            main.appendChild(isCorrect);
+            questionNum++;
+            nextQuestion();
         }
     }
 })
